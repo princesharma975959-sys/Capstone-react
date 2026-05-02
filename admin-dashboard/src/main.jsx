f34a20dc5a5da21a82b4fbd96cb11d2b701b9dec
Priyansh Sharma <princesharma975959@gmail.com>
2026-05-02 22:31:26 +0530

    final routing fix

--- admin-dashboard/src/main.jsx
+++ admin-dashboard/src/main.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <AuthProvider>
       <App />
     </AuthProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
